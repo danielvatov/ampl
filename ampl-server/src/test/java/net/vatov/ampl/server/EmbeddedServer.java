89f91b7fd8a8079e91e9cd58e66bd41ac2954c61
dvatov <dvatov@vmware.com>
2013-08-16 23:58:38 +0300

    fix hardcoded path in test

--- ampl-server/src/test/java/net/vatov/ampl/server/EmbeddedServer.java
+++ ampl-server/src/test/java/net/vatov/ampl/server/EmbeddedServer.java
@@ -4,29 +4,38 @@
 package net.vatov.ampl.server;
 
 import java.io.File;
+import java.io.FileFilter;
+import java.io.FilenameFilter;
 
 import org.mortbay.jetty.Server;
 import org.mortbay.jetty.webapp.WebAppContext;
 
 public class EmbeddedServer {
 
-	private static Server server;
-	private final static int PORT = 8080;
+    private static Server server;
+    private final static int PORT = 8080;
 
-	private static void setUp() throws Exception {
+    private static void setUp() throws Exception {
 		server = new Server(PORT);
-		WebAppContext ctx = new WebAppContext(new File(
-				"target/ampl-server-0.0.1-SNAPSHOT").getCanonicalPath(),
+		File f = new File("target");
+		String[] s = f.list(new FilenameFilter() {
+            
+            @Override
+            public boolean accept(File dir, String name) {
+                return name.matches("ampl-server-\\d+\\.\\d+\\.\\d+(-SNAPSHOT)?");
+            }
+        });
+		WebAppContext ctx = new WebAppContext(f.getCanonicalPath() + File.separator + s[0],
 				"/ampl");
 		server.addHandler(ctx);
 	}
 
-	public static void start() throws Exception {
-		setUp();
-		server.start();
-	}
+    public static void start() throws Exception {
+        setUp();
+        server.start();
+    }
 
-	public static void stop() throws Exception {
-		server.stop();
-	}
+    public static void stop() throws Exception {
+        server.stop();
+    }
 }
