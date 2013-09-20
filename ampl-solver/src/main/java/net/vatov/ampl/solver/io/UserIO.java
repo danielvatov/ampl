b8c170b7f5001f3a53746a2694739101f8948182
dvatov <dvatov@vmware.com>
2013-09-20 17:05:45 +0300

    enable use of other streams besides stdio

--- ampl-solver/src/main/java/net/vatov/ampl/solver/io/UserIO.java
+++ ampl-solver/src/main/java/net/vatov/ampl/solver/io/UserIO.java
@@ -1,5 +1,7 @@
 package net.vatov.ampl.solver.io;
 
+import java.io.PrintStream;
+import java.io.InputStream;
 import java.util.List;
 
 public interface UserIO {
@@ -13,10 +15,19 @@ public interface UserIO {
     public void refreshData(Object data);
     
     public void pause(String msg);
-        
+
+    public void message(String paramString);
+
+    public void close();
+
     public class Factory {
         public static UserIO createStdUserIO() {
             return new StdUserIO(System.in, System.out);
         }
+
+        public static UserIO createStreamUserIO(InputStream in, PrintStream out) {
+            return new StdUserIO(in, out);
+        }
     }
+
 }
